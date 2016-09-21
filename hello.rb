d1413311502e2fb0acc7cf9a893024b91429ca3a
anthonyproctor <anthonyproctor@gmail.com>
2016-09-21 19:16:30 -0400

    First rev

---
+++ hello.rb
@@ -0,0 +1,6 @@
+# hello.rb, written by Anthony J. Proctor
+# This program follows the instructions from the
+# Firehose Intro course at the Firehose Project.
+
+puts "Today, I wrote code"
+puts "and I'm a coding machine..."
